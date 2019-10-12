ee1ae703b861b589a0ebedf4b98cf9c1766cf2d6
Bo Liu <bo.robotics@gmail.com>
2019-10-12 14:52:46 -0400

    [Algo] add SlidingWindow MinSubArrayGivenSum

---
+++ AlgoPatterns/SlidingWindow/MinSubArrayGivenSum/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+#include "Output.hpp"
+
+class MinSizeSubArraySum
+{
+public:
+    static int findMinSubArray(int S, const std::vector<int> &arr)
+    {
+        int windowSum = 0, minLength = std::numeric_limits<int>::max();
+        int windowStart = 0;
+        for (int windowEnd = 0; windowEnd < arr.size(); ++windowEnd)
+        {
+            windowSum += arr[windowEnd]; // add the next element
+            // shrink the window as small as possible until the 'windowSum' is smaller than 'S'
+            while (windowSum >= S)
+            {
+                minLength = std::min(minLength, windowEnd - windowStart + 1);
+                windowSum -= arr[windowStart]; // subtract the element going out
+                windowStart++;                 // slide the window ahead
+            }
+        }
+
+        return minLength == std::numeric_limits<int>::max() ? 0 : minLength;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> arr1{2, 1, 5, 2, 3, 2};
+    int result = MinSizeSubArraySum::findMinSubArray(7, arr1);
+    std::cout << arr1;
+    std::cout << "Given sum 7, Smallest subarray length: " << result << std::endl;
+    result = MinSizeSubArraySum::findMinSubArray(7, std::vector<int>{2, 1, 5, 2, 8});
+    std::cout << "Given sum 7, Smallest subarray length: " << result << std::endl;
+    result = MinSizeSubArraySum::findMinSubArray(8, std::vector<int>{3, 4, 1, 1, 6});
+    std::cout << "Given sum 7, Smallest subarray length: " << result << std::endl;
+}
---
+++ AlgoPatterns/Util/Output.hpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const std::vector<T> &arr)
+{
+    out << "Array of " << arr.size() << " elements: ";
+    for (auto &ele : arr)
+    {
+        out << ele << ", ";
+    }
+    out << std::endl;
+
+    return out;
+}
