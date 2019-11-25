04c5c7b35f59073ea10ecea3f7a1907905ca4d33
Bo Liu <bo.robotics@gmail.com>
2019-11-24 20:22:48 -0500

    [AlgoPatterns] add NoRepeatSubstring

---
+++ AlgoPatterns/SlidingWindow/NoRepeatSubstring/main.cpp
@@ -0,0 +1,71 @@
+/**
+ * Given an array of positive numbers and a positive number ‘S’, 
+ * find the length of the smallest contiguous subarray whose sum 
+ * is greater than or equal to ‘S’. Return 0, if no such subarray exists.
+ */
+
+#include <iostream>
+#include <limits>
+#include <string>
+#include <deque>
+#include <set>
+#include "Output.hpp"
+
+class NoRepeatSubstring
+{
+public:
+    static size_t findLength(const std::string &str, std::deque<char> &outString)
+    {
+        size_t maxLen{0};
+        std::deque<char> stringWindow{};
+        std::set<char> charSet{};
+
+        for (const char &ch : str)
+        {
+            if (charSet.count(ch) > 0)
+            {
+
+                while (true)
+                {
+                    auto frontChar = std::move(stringWindow.front());
+                    stringWindow.pop_front();
+                    charSet.erase(frontChar);
+                    if (frontChar == ch)
+                    {
+                        stringWindow.push_back(ch);
+                        charSet.insert(ch);
+                        break;
+                    }
+                }
+                continue;
+            }
+
+            stringWindow.push_back(ch);
+            charSet.insert(ch);
+
+            if (stringWindow.size() > maxLen)
+            {
+                maxLen = stringWindow.size();
+                outString = stringWindow;
+            }
+        }
+
+        return maxLen;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    std::deque<char> out;
+    std::cout << " \"aabccbb\": " << NoRepeatSubstring::findLength("aabccbb", out)
+              << std::endl;
+    std::cout << out;
+
+    std::cout << " \"abbbb\": " << NoRepeatSubstring::findLength("abbbb", out)
+              << std::endl;
+    std::cout << out;
+
+    std::cout << " \"abccde\": " << NoRepeatSubstring::findLength("abccde", out)
+              << std::endl;
+    std::cout << out;
+}
